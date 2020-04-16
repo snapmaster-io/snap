f6d32dc64935e4fc1a1e11c490d4b33b409413c9
Omri Gazitt <ogazitt@gmail.com>
2020-04-15 21:21:36 -0700

    finalized versioning implementation

--- pkg/version/init.go
+++
@@ -1,4 +0,0 @@
-package version
-
-Version := "0.1.0"
-var GitHash string
---
+++ pkg/version/version.go
@@ -0,0 +1,7 @@
+package version
+
+// Version number
+var Version string = "0.1.0"
+
+// GitHash of the last commit
+var GitHash string
